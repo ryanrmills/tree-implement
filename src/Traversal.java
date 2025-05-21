f164907dd3434d44e573e5289f76c97d43ad9477
ryanrmills <ryanroymills@gmail.com>
2025-05-21 11:46:13 -0700

    finished

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,36 @@
 public class Traversal {
   public static void main(String[] args) {
+    TreeNode root = new TreeNode(10);
+
+    System.out.println(root.data);
+
+    root.left = new TreeNode(9);
+    root.right = new TreeNode(15);
+
+    root.left.left = new TreeNode(5);
+    root.left.right = new TreeNode(2);
+
+    root.right.left = new TreeNode(-3);
+    root.right.right = new TreeNode(5);
+
+    root.right.right.right = new TreeNode(22);
+  }
+
+  public static void preOrder(TreeNode current){
+
+    if (current == null){
+      return;
+    }
+
+    System.out.println(current.data);
+
+    if (current.left != null){
+      preOrder(current.left);
+    }
+
+    if (current.right != null){
+      preOrder(current.right);
+    }
 
   }
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,9 @@
 public class TreeNode {
-
-  
+    public int data;
+    public TreeNode left;
+    public TreeNode right;
+    
+    public TreeNode(int data){
+        this.data = data;
+    }
 }
