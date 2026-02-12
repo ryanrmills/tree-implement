dbc6d05b225fcff5e3dfa08822086a246e1d883e
ryanrmills <ryanroymills@gmail.com>
2026-02-12 13:56:25 -0800

    learned about tree traversals

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,35 +1,35 @@
 public class Traversal {
   public static void main(String[] args) {
-    TreeNode root = new TreeNode(10);
+    TreeNode rooot = new TreeNode(10);
 
-    System.out.println(root.data);
+    System.out.println(rooot.data);
 
-    root.left = new TreeNode(9);
-    root.right = new TreeNode(15);
+    rooot.left = new TreeNode(9);
+    rooot.right = new TreeNode(15);
 
-    root.left.left = new TreeNode(5);
-    root.left.right = new TreeNode(2);
+    rooot.left.left = new TreeNode(5);
+    rooot.left.right = new TreeNode(2);
 
-    root.right.left = new TreeNode(-3);
-    root.right.right = new TreeNode(5);
+    rooot.right.left = new TreeNode(-3);
+    rooot.right.right = new TreeNode(5);
 
-    root.right.right.right = new TreeNode(22);
+    rooot.right.right.right = new TreeNode(22);
   }
 
-  public static void preOrder(TreeNode current){
+  public static void preOrder(TreeNode current1){
 
-    if (current == null){
+    if (current1 == null){
       return;
     }
 
-    System.out.println(current.data);
+    System.out.println(current1.data);
 
-    if (current.left != null){
-      preOrder(current.left);
+    if (current1.left != null){
+      preOrder(current1.left);
     }
 
-    if (current.right != null){
-      preOrder(current.right);
+    if (current1.right != null){
+      preOrder(current1.right);
     }
 
   }
